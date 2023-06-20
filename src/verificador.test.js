5bb3eb6b5dfcc7d3d3dd15bc10e9a714a15d0717
Oswi142 <edwin.roman@ucb.edu.bo>
2023-06-20 10:29:13 -0400

    Hacemos una prueba tdd con letras y 2 corchetes

--- src/verificador.test.js
+++ src/verificador.test.js
@@ -16,4 +16,7 @@ describe("Verificar", () => {
   it("Debería devolver incorrecto", () => {
     expect(verificar("[[][")).toEqual("Incorrecto");
   });
+  it("Debería devolver correcto", () => {
+    expect(verificar("[[Hola]]")).toEqual("Correcto");
+  });
 });
