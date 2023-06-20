4a97eb05710be5daf86e8dff3e75c3976d253eb0
Oswi142 <edwin.roman@ucb.edu.bo>
2023-06-20 10:01:09 -0400

    Hacemos 2 pruebas TDD ambas corriendo correctamente

--- src/verificador.test.js
+++ src/verificador.test.js
@@ -4,4 +4,7 @@ describe("Verificar", () => {
   it("Debería devolver correcto", () => {
     expect(verificar("[]")).toEqual("Correcto");
   });
+  it("Debería devolver incorrecto", () => {
+    expect(verificar("[[")).toEqual("Incorrecto");
+  });
 });
