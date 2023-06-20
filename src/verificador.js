5ef41582d55b684ae80a1064e5119abf02fd868f
Oswi142 <edwin.roman@ucb.edu.bo>
2023-06-20 10:26:32 -0400

    Realizamos un código alterntivo

--- src/verificador.js
+++ src/verificador.js
@@ -9,7 +9,18 @@ function verificar(cadena) {
     if (letras[1] == "[" && letras[letras.length - 2] == "]" && letras[0] == "[" && letras[letras.length - 1] == "]") {
       resp = "Correcto";
     }
-  }
+  }/*
+  for(var i=0;i<letras.length;i++)
+  {
+    if(letras[i]=="[")
+    {
+      for(var j=i+1;j<letras.length;j++)
+      {
+        if(letras[j]=="]")
+        resp="Correcto";
+      }
+    }
+  }*/
   return resp;
 }
 export default verificar;
